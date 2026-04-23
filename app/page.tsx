528d7cad14ab48325b318777778ca4daa91c526d
Fuz483 <62098382+Fuz483@users.noreply.github.com>
2026-04-23 17:37:59 +0300

    Updated page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Проект для хакатона MTBank 🚀</h1>
+      <p>Если вы видите этот текст, значит Next.js успешно запущен!</p>
+    </main>
+  );
+}
